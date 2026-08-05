c19fbc4f5c8525de1a5fd9a1f3cac169c0118c6c
sendemailtojesse-stack <sendemailtojesse@gmail.com>
2026-08-05 18:06:22 -0500

    Add files via upload

--- fetch-data.js
+++ fetch-data.js
@@ -580,7 +580,7 @@ async function fetchHighUtilityMatrix() {
     // Assign per-section rank restarting at 1 for each category
     const sectionCounters = {};
     orderedGrid.forEach(item => {
-        const key = item.category + (item.searchLabel ? '-' + item.searchLabel : '');
+        const key = item.category;
         if (!sectionCounters[key]) sectionCounters[key] = 0;
         sectionCounters[key]++;
         item.rank = sectionCounters[key];
